3df319bb1e5f17ec730b3d658b2a8ebb41c831d2
Chaerul Umam <chaerulumamb244@gmail.com>
2022-08-30 23:00:36 +0700

    api get Category done

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,5 +1,6 @@
 const Player = require("./model");
 const Voucher = require("../voucher/model");
+const Category = require("../category/model");
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -29,6 +30,22 @@ module.exports = {
       }
 
       res.status(200).json({ data: voucher });
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: err.message || `Internal server error!` });
+    }
+  },
+
+  category: async (req, res) => {
+    try {
+      const category = await Category.find();
+
+      if (!category) {
+        return res.status(404).json({ message: "Category not found!" });
+      }
+
+      res.status(200).json({ data: category });
     } catch (err) {
       res
         .status(500)
