6deb570bc0ea88df62983238c62c6e9e0d09b966
Chaerul Umam <chaerulumamb244@gmail.com>
2022-09-05 22:50:46 +0700

    api get dashboard, profile well done

--- app/player/router.js
+++ app/player/router.js
@@ -7,6 +7,9 @@ const {
   category,
   checkout,
   history,
+  historyDetail,
+  dashboard,
+  profile,
 } = require("./controller");
 
 router.get("/landingpage", landingPage);
@@ -14,5 +17,8 @@ router.get("/:id/detail", detailPage);
 router.get("/category", category);
 router.post("/checkout", isLoginPlayer, checkout);
 router.get("/history", isLoginPlayer, history);
+router.get("/history/:id/detail", isLoginPlayer, historyDetail);
+router.get("/dashboard", isLoginPlayer, dashboard);
+router.get("/profile", isLoginPlayer, profile);
 
 module.exports = router;
