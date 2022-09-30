ef2ff3a657e92888690579992daa2839fc8654fc
Chaerul Umam <chaerulumamb244@gmail.com>
2022-09-30 19:19:44 +0700

    edit api

--- app/player/controller.js
+++ app/player/controller.js
@@ -96,7 +96,9 @@ module.exports = {
       const payload = {
         historyVoucherTopup: {
           gameName: res_voucher._doc.name,
-          category: res_voucher._doc.category ? res_voucher._doc.category : "",
+          category: res_voucher._doc.category
+            ? res_voucher._doc.category.name
+            : "",
           thumbnail: res_voucher._doc.thumbnail,
           coinName: res_nominal._doc.coinName,
           coinQuantity: res_nominal._doc.coinQuantity,
@@ -281,6 +283,7 @@ module.exports = {
 
         src.on("end", async () => {
           let player = await Player.findOne({ _id: req.player._id });
+
           let currentImage = `${config.rootPath}/public/uploads/${player.avatar}`;
           if (fs.existsSync(currentImage)) {
             fs.unlinkSync(currentImage);
